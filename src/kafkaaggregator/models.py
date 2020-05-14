102a84867c271ef09413df56d4e9e41f4d7f2c60
Angelo Fausti <afausti@lsst.org>
2020-05-14 10:32:48 -0700

    Add class factory for creating records at runtime
    - This class factory is going to be used for creating the Faust records for the aggregated topics at runtime
    - Use type() mechanism for creating a new class with __annotations__, see https://github.com/robinhood/faust/blob/master/faust/models/record.py#L132

---
+++ src/kafkaaggregator/models.py
@@ -0,0 +1,41 @@
+"""A class factory for Faust Records."""
+
+__all__ = ["make_record"]
+
+from typing import Any, Mapping
+
+import faust_avro
+
+
+def make_record(
+    cls_name: str, fields: Mapping[str, Any], doc: str = None
+) -> faust_avro.Record:
+    """A class factory for Faust Records.
+
+    Examples
+    --------
+    >>> Foo = make_record('Foo', {'bar': int})
+    >>> f = Foo(bar=0)
+    >>> f.bar
+    0
+    >>> f.dumps()
+    {'bar': 0, '__faust': {'ns': '__main__.Foo'}}
+
+    Parameters
+    ----------
+    cls_name: `str`
+        Name of the new class to create.
+    fields: `dict`
+        Dictionary mapping of field names and types for the Faust Record.
+    doc: `str`
+        Docstring for the new class.
+
+    Returns
+    -------
+    cls: `faust_avro.Record`
+        A faust_avro.Record subclass.
+
+    """
+    cls_attrs = dict(__annotations__=fields, __doc__=doc,)
+
+    return type(cls_name, (faust_avro.Record,), cls_attrs)
