6369bd7634537f20cab825d60ce8b4de4a01bc9c
Angelo Fausti <afausti@lsst.org>
2020-05-21 11:24:48 -0700

    Implement tests
    - Add tests for `Aggregator._create_aggregation_fields()`
    - Add tests for `models.make_record()`
    - Remove test for the example as it is not very meaningful
    - Use pytest-vcr plugin to mock request/response to the Schema Registry
    - Add test to `Topic.register()`
    - Add test to `Topic.get_fields()`

--- tests/example/test_example.py
+++ tests/example/test_example.py
@@ -1,18 +1,5 @@
 import pytest
-from faust import Record
 from faust.windows import TumblingWindow
-
-from kafkaaggregator.config import Configuration
-from kafkaaggregator.example.agents import count, process_src_topic
-
-config = Configuration()
-
-
-class TestTopic(Record):
-
-    __test__ = False
-    time: float
-    value: float
 
 
 @pytest.mark.asyncio
@@ -29,18 +16,3 @@ async def test_tumbling_window_ranges():
     # Test if a timestamp between (start + end) and (start + size) falls
     # in the expected window
     assert window.ranges(start + end + 0.05) == [(start, end)]
-
-
-@pytest.mark.asyncio
-async def test_count_table(test_app):
-    """Test if the count table is updated every time the agent processes a new
-    message and if counts are persisted.
-    """
-    test_topic = TestTopic(time=0.0, value=0.5)
-    async with process_src_topic.test_context() as agent:
-        await agent.put(test_topic)
-        assert count[config.src_topic] == 1
-
-    async with process_src_topic.test_context() as agent:
-        await agent.put(test_topic)
-        assert count[config.src_topic] == 2
---
+++ tests/kafkaaggregator/test_aggregator.py
@@ -0,0 +1,58 @@
+import pytest
+
+from kafkaaggregator.aggregator import Aggregator
+from kafkaaggregator.fields import Field
+from kafkaaggregator.models import make_record
+
+
+@pytest.fixture
+def source_topic_fields():
+    fields = [
+        Field("time", int),
+        Field("value", float),
+        Field("excluded", int),
+        Field("nonnumeric", bool),
+        Field("inttype", int),
+    ]
+    return fields
+
+
+@pytest.fixture
+def excluded_field_names():
+    return ["time", "excluded"]
+
+
+def test_aggregation_fields(source_topic_fields, excluded_field_names):
+    """Test aggregation fields creation."""
+    aggregation_fields = Aggregator._create_aggregation_fields(
+        source_topic_fields, excluded_field_names
+    )
+    # `time`, `count` and `window_size` are added by the aggregator
+    assert Field("time", float) in aggregation_fields
+    assert Field("count", int) in aggregation_fields
+    assert Field("window_size", float) in aggregation_fields
+    # if there's `time` field in the source topic it is replaced
+    assert Field("time", int) not in aggregation_fields
+    # summary statistic fields added based on the the `value` field
+    assert Field("min_value", float) in aggregation_fields
+    assert Field("mean_value", float) in aggregation_fields
+    assert Field("median_value", float) in aggregation_fields
+    assert Field("max_value", float) in aggregation_fields
+    # field names added to the excluded_field_names list are not aggregated
+    assert Field("excluded", float) not in aggregation_fields
+    # non numeric fields are excluded
+    assert Field("nonnumeric", bool) not in aggregation_fields
+    assert Field("min_nonnumeric", float) not in aggregation_fields
+    # int type is aggregated as float
+    assert Field("min_inttype", float) in aggregation_fields
+
+
+def test_record_class():
+    """Test Faust Record creation."""
+    # make a simple Faust Record
+    Foo = make_record(
+        cls_name="Foo", fields=[Field("bar", int)], doc=f"Test record",
+    )
+    f = Foo(bar=0)
+    assert f.is_valid()
+    assert f.asdict() == {"bar": 0}
