0178bc0fd2a1e2441fdcc02812ead5ec6d553dc7
Angelo Fausti <afausti@lsst.org>
2020-06-15 18:30:29 -0700

    Implement the AgentGenerator class
    - Generate the agents code from a list of source topics

---
+++ src/kafkaaggregator/generator.py
@@ -0,0 +1,110 @@
+"""Generates Faust agents based on the agents.j2 template."""
+
+__all__ = ["AgentGenerator"]
+
+import logging
+import os
+from typing import Any, List, Mapping
+
+import aiofiles
+from jinja2 import Environment, PackageLoader, Template, TemplateError
+
+from kafkaaggregator.app import config
+
+logger = logging.getLogger("kafkaaggregator")
+
+
+class AgentGenerator:
+    """Generate Faust agents from a list of source topics.
+
+    Creates the context and renders the agents code template.
+
+    Parameters
+    ----------
+    source_topic_names : `list`
+        List of source topic names.
+    """
+
+    logger = logger
+
+    def __init__(self, source_topic_names: List[str]) -> None:
+        self._source_topic_names = source_topic_names
+        self._template: Template = self._load_template()
+
+    @property
+    def template(self) -> Template:
+        """Get the agents template."""
+        return self._template
+
+    @staticmethod
+    def _create_filepath(source_topic_name: str) -> str:
+        """Return the file path for the agents code.
+
+        The directory name comes from the agents_output_dir configuration
+        parameter and the file name is based on source topic name.
+
+        Parameters
+        ----------
+        source_topic_name : `str`
+            Name of the source topic to aggregate.
+        """
+        agents_output_dir = config.agents_output_dir
+
+        filepath = os.path.join(agents_output_dir, f"{source_topic_name}.py")
+
+        return filepath
+
+    @staticmethod
+    def _create_context(source_topic_name: str) -> Mapping[str, Any]:
+        """Create the template context.
+
+        The template context stores the values passed to the template.
+
+        Parameters
+        ----------
+        source_topic_name : `str`
+            Name of the source topic to aggregate
+
+        Returns
+        -------
+        context : `dict`
+            A dictionary with values passed to the template.
+        """
+        topic_rename_format = config.topic_rename_format
+
+        aggregation_topic_name = topic_rename_format.format(
+            source_topic_name=source_topic_name
+        )
+
+        context = dict(
+            source_topic_name=source_topic_name,
+            aggregation_topic_name=aggregation_topic_name,
+        )
+
+        return context
+
+    @staticmethod
+    def _load_template() -> Template:
+        """Load the agents template file."""
+        agents_template_file = config.agents_template_file
+
+        env = Environment(
+            loader=PackageLoader("kafkaaggregator"), keep_trailing_newline=True
+        )
+        try:
+            template = env.get_template(agents_template_file)
+        except TemplateError as e:
+            logger.error("Error loading the agents template file.")
+            raise e
+
+        return template
+
+    async def run(self) -> None:
+        """Run agents code generation."""
+        for source_topic_name in self._source_topic_names:
+            logger.info(f"Generating agent code for {source_topic_name}.")
+            filepath = self._create_filepath(source_topic_name)
+            context = self._create_context(source_topic_name)
+
+            async with aiofiles.open(filepath, "w") as file:
+                await file.write(self._template.render(**context))
