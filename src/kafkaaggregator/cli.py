e0c5f6852877f2c31972d1933476099a04269db4
Angelo Fausti <afausti@lsst.org>
2020-06-23 11:34:11 -0700

    Use the AggregationExample class in the cli
    - The init-example and produce commands can now use the AggregationExample class

--- src/kafkaaggregator/cli.py
+++ src/kafkaaggregator/cli.py
@@ -2,17 +2,13 @@
 
 __all__ = ["main", "produce", "init_example"]
 
-import asyncio
-import json
 import logging
-import random
-from time import time
 
 from faust.cli import AppCommand, option
 
-from kafkaaggregator.app import app, config
+from kafkaaggregator.app import app
+from kafkaaggregator.example import AggregationExample
 from kafkaaggregator.generator import AgentGenerator
-from kafkaaggregator.topics import SourceTopic
 
 logger = logging.getLogger("kafkaaggregator")
 
@@ -41,53 +37,18 @@ def main() -> None:
 async def produce(
     self: AppCommand, frequency: float, max_messages: int
 ) -> None:
-    """Produce messages for the kafkaaggregator source topic."""
-    # Assume the source topic exists
-    src_topic = app.topic(config.source_topic_name)
-
-    logger.info(
-        f"Producing {max_messages} message(s) for {config.source_topic_name} "
-        f"at {frequency} Hz."
+    """Produce messages for the aggregation example."""
+    example = AggregationExample()
+    await example.produce(
+        app=app, frequency=frequency, max_messages=max_messages
     )
-
-    for i in range(max_messages):
-        value = random.random()
-        await src_topic.send(value=dict(time=time(), value=value))
-        await asyncio.sleep(1 / frequency)
 
 
 @app.command()
 async def init_example(self: AppCommand) -> None:
-    """Initialize the source topic used in the aggregation example.
-
-    The source topic has a fixed schema with a timestamp and a value. It is
-    used produce messages for the aggregation example. This topic is not
-    managed by Faust, it represents an external topic and thus needs to be
-    created in Kafka and its schema needs to registered in the Schema Registry.
-    """
-    source_topic = SourceTopic(name=config.source_topic_name)
-
-    schema = json.dumps(
-        dict(
-            type="record",
-            name="SourceTopic",
-            doc="Source topic with raw values.",
-            fields=[
-                dict(name="time", type="double"),
-                dict(name="value", type="double"),
-            ],
-        )
-    )
-
-    # Register source topic schema
-    await source_topic.register(schema=schema)
-
-    # Declare the source topic as an external topic in Faust. External topics
-    # do not have a corresponding model in Faust, thus value_type=bytes.
-    external_topic = app.topic(
-        source_topic.name, value_type=bytes, internal=False
-    )
-    await external_topic.declare()
+    """Initialize the source topic used in the aggregation example."""
+    example = AggregationExample()
+    await example.initialize(app=app)
 
 
 @app.command()
