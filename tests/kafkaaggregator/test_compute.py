f11d70316d787e1f747acae991856abcec172e81
Angelo Fausti <afausti@lsst.org>
2020-06-24 11:42:27 -0700

    Add test for the min_sample_size configuration

--- tests/kafkaaggregator/test_compute.py
+++ tests/kafkaaggregator/test_compute.py
@@ -52,10 +52,31 @@ def expected_result() -> Mapping[str, Any]:
     return result
 
 
+@pytest.fixture
+def first_message_value(incoming_messages: List[Any]) -> Mapping[str, Any]:
+    """Return the value of the first message instead of computing statistics.
+
+    That's the expected result if the number of messages in the aggregation
+    window is smaller than the min_sample_size.
+    """
+    result = {
+        "count": 3,
+        "min_value": incoming_messages[0]["value"],
+        "time": 1.0,  # timestamp of the aggregated message
+        "window_size": 1.0,
+        "max_value": incoming_messages[0]["value"],
+        "mean_value": incoming_messages[0]["value"],
+        "median_value": incoming_messages[0]["value"],
+        "stdev_value": incoming_messages[0]["value"],
+    }
+    return result
+
+
 def test_compute(
     incoming_messages: List[Any],
     aggregation_fields: List[Field],
     expected_result: Mapping[str, Any],
+    first_message_value: Mapping[str, Any],
 ) -> None:
     """Test the Aggregator compute method.
 
@@ -88,7 +109,17 @@ def test_compute(
         time=1.0,
         window_size=1.0,
         min_sample_size=1,
-        messages=incoming_messages
+        messages=incoming_messages,
     )
     assert aggregated_message.is_valid()
     assert aggregated_message.asdict() == expected_result
+
+    # Test the case where the min_sample_size is larger than the number
+    # of messages in the aggregation window
+    aggregated_message = Agg.compute(
+        time=1.0,
+        window_size=1.0,
+        min_sample_size=5,
+        messages=incoming_messages,
+    )
+    assert aggregated_message.asdict() == first_message_value
