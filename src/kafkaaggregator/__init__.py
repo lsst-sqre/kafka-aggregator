101d963b44f868c1c554ff3eac786ecc74b14685
Angelo Fausti <afausti@lsst.org>
2020-07-27 15:23:56 -0700

    Run with isort 5.1.4

--- src/kafkaaggregator/__init__.py
+++ src/kafkaaggregator/__init__.py
@@ -5,9 +5,9 @@ __all__ = ["__version__", "metadata"]
 import sys
 
 if sys.version_info < (3, 8):
-    from importlib_metadata import version, PackageNotFoundError
+    from importlib_metadata import PackageNotFoundError, version
 else:
-    from importlib.metadata import version, PackageNotFoundError
+    from importlib.metadata import PackageNotFoundError, version
 
 
 __version__: str
